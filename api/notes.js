2572ed5c54bae31e587b1aefb217c0f40fc15835
ckaushik1799 <104199443+ckaushik1799@users.noreply.github.com>
2022-07-27 22:36:30 +0530

    Create notes.js

---
+++ api/notes.js
@@ -0,0 +1,4 @@
+// queries in js, ie they help in routing .
+// in the project, we need queries to fetch all the post, or posts according to categories or user.
+// to use queries, we just have to write const var name for a particular query.
+// eg. const username = req.query.user // ie the endpoint of API is given as  /?user= value, and username holds the value of this query in the endpoint.
